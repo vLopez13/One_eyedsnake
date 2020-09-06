73b962f4e3320347604c5c6eca84a524caabcc6d
Vanessa <14098523+vLopez13@users.noreply.github.com>
2020-09-05 17:40:17 -0700

    Update PairOfDice.java

--- PairOfDice.java
+++ PairOfDice.java
@@ -1,17 +1,14 @@
 //get die class and place more comments 
 //import die.java; connect with die.java
 //PairOfDice class
-class PairOfDice extends Die
-{
-	//variable declaration
-   
-	private Die die1, die2;
+class PairOfDice 
+{  
+	
 	//constructor
-	public PairOfDice()
-	{
+	
 		Die die1 = new Die();
 		Die die2 = new Die();
-	}
+	
 
 	//Rolling two dice
 	public int roll()
@@ -24,6 +21,30 @@ class PairOfDice extends Die
 	{
 		return die1.getFaceValue() + die2.getFaceValue();
 	}
+	public boolean roll_double()
+        { if(die0.getFaceValue() == 1 && die1.getFaceValue()==1)
+           return true;
+     
+          if(die0.getFaceValue() == 2 && die1.getFaceValue()==2) 
+           return true;
+     
+          if(die0.getFaceValue() == 3 && die1.getFaceValue()==3)
+          return true;
+	 
+         if(die0.getFaceValue() == 4 && die1.getFaceValue()==4)
+          return true;
+	 
+         if(die0.getFaceValue() == 5 && die1.getFaceValue()==5)
+         return true;
+	 
+         if(die0.getFaceValue() == 6 && die1.getFaceValue()==6)
+          return true;
+	 
+        else{
+     
+         return false;
+      }
+   }
 
 	//updating the value of die1
 	public void setDie1FaceValue(int value)
